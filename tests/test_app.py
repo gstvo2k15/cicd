c44cc1771f8adf27caa35641da864b0f08b9ef08
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-06-12 21:58:02 +0200

    Fixed pylint for initial steps-2

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,17 @@
 import unittest
 from app.main import app
 
+
 class BasicTestCase(unittest.TestCase):
+    """Basic test cases."""
+
     def test_home(self):
+        """Test the home page."""
         tester = app.test_client(self)
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Hello, World!')
+
 
 if __name__ == '__main__':
     unittest.main()
