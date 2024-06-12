84989a49e796fddf2e9c721e2f6b13ade92fbbfb
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-06-12 21:27:50 +0200

    Added fix for pylint-1a

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,10 @@ import unittest
 from app.main import app
 
 class BasicTestCase(unittest.TestCase):
+    """Basic test cases."""
+
     def test_home(self):
+        """Test the home page."""
         tester = app.test_client(self)
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
