edaec613fff292f5885b0dfbbc14036590d58b00
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-07-21 10:40:37 +0200

    Added fixes for test_app.py for fix issues-1b

--- app/python/tests/test_app.py
+++ app/python/tests/test_app.py
@@ -1,11 +1,18 @@
-import pytest
-from app.main import app
+import sys
+import os
+import unittest
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
+# Verificar que el directorio 'app/python' esté en sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-def test_hello(client):
-    rv = client.get('/')
-    assert rv.data == b'Hello, World!'
+from main import app
+
+class TestMain(unittest.TestCase):
+    def test_hello(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
